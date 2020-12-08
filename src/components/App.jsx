2d839a4ff8c0f93e10f0a9a6f086383f3ad31d7f
scurry222 <scurry222@gmail.com>
2020-12-07 21:17:56 -0800

    Add video player component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,12 +10,12 @@ export default class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>Search</em><Search video={exampleVideoData}/></h5></div>
+            <div><h5><em>Search</em><Search /></h5></div>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><h5><em></em><VideoPlayer /></h5></div>
+            <div><h5><em></em><VideoPlayer video={exampleVideoData[0]} /></h5></div>
           </div>
           <div className="col-md-5">
             <div><h5><em></em><VideoList videos={exampleVideoData} /></h5></div>
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,13 @@
-var VideoPlayer = ({video}) => (
+var VideoPlayer = (props) => (
   <div className="video-player">
+    {console.log(props.video)}
+
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={video.snippet.thumbnails.default.url} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={"https://www.youtube.com/embed/" + props.video.id.videoId} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
