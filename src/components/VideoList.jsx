d5227a670b0313609068fd9b7b4b10458fb0c234
scurry222 <scurry222@gmail.com>
2020-12-08 17:17:07 -0800

    Add GET request to gather embeddable videos

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,9 +1,9 @@
 import VideoListEntry from './VideoListEntry.js';
 
-var VideoList = (props) => 
+var VideoList = ({click, videos}) => 
   <div className="video-list">
-    {props.videos.map((video, i) => 
-      <VideoListEntry click={props.click} key={i} video={video}/>
+    {videos.map((video, i) => 
+      <VideoListEntry click={click} key={i} video={video}/>
       // <div key={i}><h5><em>{video.snippet.title}</em></h5></div>
     )}
   </div>;
@@ -14,10 +14,6 @@ VideoList.propTypes = {
   videos: PropTypes.array.isRequired
 };
 
-/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
-      <div><h5><em>videoListEntry</em> view goes here</h5></div>
-      <div><h5><em>videoListEntry</em> view goes here</h5></div>
-      <div><h5><em>videoListEntry</em> view goes here</h5></div> */
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var options = {
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    part: 'snippet',
+    videoEmbeddable: 'true',
+    type: 'video'
+  };
+  $.get('https://www.googleapis.com/youtube/v3/search', options, ((page) => {
+    callback(page.items);
+  }));
 };
 
 export default searchYouTube;
